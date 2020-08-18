e9bfba64872042aca6c0ab4ece851922280998d4
Srini <Srini@LAPTOP-Q9TDGPU1>
2020-08-18 21:46:10 +0530

    Updated

--- MyTestAutomation/TestClass.java
+++ MyTestAutomation/TestClass.java
@@ -1,9 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class TestClass {
 	
-@BeforeTest
-public void login() throws Exception {
-	System.setProperty("webdriver.Chrome.driver", "");
+
+public static void login() throws Exception {
+	
+	System.setProperty("webdriver.chrome.driver","D:\\eclipse\\drivers\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
+	driver.navigate().to("https://google.com");
+	driver.manage().window().maximize();
+	Thread.sleep(2000);
+	System.out.println(driver.getTitle());
+	driver.close();
 	}
+public static void main (String[] args) throws Exception
+{
+	login();
+}
 }
